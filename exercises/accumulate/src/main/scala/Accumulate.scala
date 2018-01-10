1e8f7903c3a944e029775bb38b3582c08e9763ab
Marek Żebrowski <marek.zebrowski@gmail.com>
2018-01-10 20:14:30 +0100

    stack-safe accumulate

--- exercises/accumulate/src/main/scala/Accumulate.scala
+++ exercises/accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,25 @@
+import scala.annotation.tailrec
+
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = ???
+  // stack safe accumulate - extra pass on reverse
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+
+    @tailrec
+    def acc0(l: List[A], r:List[B]):List[B] = {
+      l match {
+        case Nil    => r
+        case h :: t => acc0(t, f(h) :: r)
+      }
+    }
+
+    @tailrec
+    def reverse(l: List[B], r:List[B]):List[B] = {
+      l match {
+        case Nil => r
+        case h :: t => reverse(t, h :: r)
+      }
+    }
+
+    reverse(acc0(list, Nil), Nil)
+  }
 }
